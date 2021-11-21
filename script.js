b2a89e967037dad81693ea371c1ef91274f9b73d
pasha2708 <pasha2708@gmail.com>
2021-11-21 15:55:59 +0200

    Bugs fixed

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
+const firstNumber = document.querySelector("#firstNumber");
+      lastNumber = document.querySelector("#lastNumber");
+      resultText = document.querySelector("#result_value");
+      button = document.querySelector("#button");
+      listBan = document.querySelector("#banList")
+      clearBanListBtn = document.querySelector('#clear_banlist')
+      clearResultsBtn = document.querySelector('#clear_results')
+      results = document.querySelector('#results')
 let prevResult = 0;
 
 button.addEventListener('click', getRandom)
+clearBanListBtn.addEventListener('click', clearBanList)
+clearResultsBtn.addEventListener('click', clearResults)
 
 function getRandom() {
-    const firstNumber = document.querySelector("#firstNumber");
-    const lastNumber = document.querySelector("#lastNumber");
-    const resultText = document.querySelector("#result_value");
-    const button = document.querySelector("#button");
-    const listBan = document.querySelector("#banList")
-    const results = document.querySelector('#results')
+    firstNumber.classList.remove('error')
+    lastNumber.classList.remove('error')
+
+    if (firstNumber.value == '' || isNaN(firstNumber.value)
+        || lastNumber.value == '' || isNaN(lastNumber.value)){
+        console.log(false);
+        firstNumber.classList.add('error')
+        lastNumber.classList.add('error')
+        return;
+    }
 
     let arrayBan = listBan.value.split('\n');
         let random = Math.round(firstNumber.value - 0.5 + Math.random() * (lastNumber.value - firstNumber.value + 1));
@@ -21,6 +35,14 @@ function getRandom() {
     prevResult = random;
     results.value += (`${random}\n`);
     results.focus();
+}
+
+function clearBanList() {
+    listBan.value = "";
+}
+
+function clearResults() {
+    results.value = "";
 }
 
 
